5dbd0fb1671cc0dcfb5dd0369dc4d78081b3ef80
MaZeSA <lostfilmclient@gmail.com>
2021-08-24 21:28:41 +0300

    linq

--- Semestr3/Home/06 Linq/linq/linq/Program.cs
+++ Semestr3/Home/06 Linq/linq/linq/Program.cs
@@ -28,29 +28,103 @@ namespace linq
         static void Main(string[] args)
         {
             Console.WriteLine($"iнформацiю про столицi(назву, країну та населення);");
-            cities.Where(x=> x.IsCapital == true).ToList().ForEach(x => Console.WriteLine($"{x.Name}  {x.Country} {x.Population}"));
+            var query1 =
+            from city in cities
+            where city.IsCapital == true
+            select city;
+            query1.ToList().ForEach(x => Console.WriteLine($"{x.Name} {x.Country} {x.Population}"));
 
             Console.WriteLine($"\nназви мiст, що мiстять букву 'i' у назвi мiста;");
-            cities.Where(x => x.Name.IndexOf("i") >= 0).ToList().ForEach(x => Console.WriteLine(x.Name));
+            query1 =
+            from city in cities
+            where city.Name.IndexOf("i") >= 0
+            select city;
+            query1.ToList().ForEach(x => Console.WriteLine(x.Name));
 
             Console.WriteLine("\nназви столиць разом iз населенням у порядку спадання населення;");
-            cities.Where(x => x.IsCapital == true).ToList().OrderByDescending(x => x.Population).ToList().ForEach(x => Console.WriteLine(x.Name + " " + x.Population));
+            query1 =
+            from city in cities
+            where city.IsCapital == true
+            orderby city.Population descending
+            select city;
+            query1.ToList().ForEach(x => Console.WriteLine(x.Name + " " + x.Population));
 
             Console.WriteLine("\nназви країн, що мiстять мiста, назви яких закiнчуютсья на букву 'w';");
-            cities.Where(x => x.Name.Last() == 'w').ToList().ForEach(x => Console.WriteLine(x.Country));
+            query1 =
+            from city in cities
+            where city.Name.Last() == 'w'
+            select city;
+            query1.ToList().ForEach(x => Console.WriteLine(x.Country));
 
             Console.WriteLine("\nназви мiст, де назва країни закiнчується на 'e', а назви мiст починаються на букву 'a';");
-            cities.Where(x => x.Country.Last() == 'e' && x.Name.First().ToString().ToLower() == "a" ).ToList().ForEach(x => Console.WriteLine(x.Name));
+            query1 =
+            from city in cities
+            where city.Country.Last() == 'w' && city.Name.ToString().ToLower() == "a"
+            select city;
+            query1.ToList().ForEach(x => Console.WriteLine(x.Name));
             Console.WriteLine("Примiтка : В даному прикладi немає жодного мiста, яке починається на 'а'");
 
             Console.WriteLine("\nвсi непарнi числа;");
-            numbers.Where(x => x % 2 != 0).ToList().ForEach(x => Console.Write(x + " "));
+            var query2 =
+            from num in numbers
+            where num % 2 != 0
+            select num;
+            query2.ToList().ForEach(x => Console.Write(x + " "));
 
             Console.WriteLine("\n\nдодатнi числа iз 'numbers' у порядку зростання;");
-            numbers.Where(x => x > 0).ToList().OrderBy(x => x).ToList().ForEach(x => Console.Write(x + " "));
-           
+            query2 =
+            from num in numbers
+            where num > 0
+            orderby num
+            select num;
+            query2.ToList().ForEach(x => Console.Write(x + " "));
+
             Console.WriteLine("\n\nвiд'ємнi числа iз 'numbers' у порядку спадання.");
-            numbers.Where(x => x < 0).ToList().OrderByDescending(x => x).ToList().ForEach(x => Console.Write(x + " "));
+            query2 =
+            from num in numbers
+            where num < 0
+            orderby num descending
+            select num;
+            query2.ToList().ForEach(x => Console.Write(x + " "));
+
+
+            Console.WriteLine("\nкiлькiсть столиць;");
+            Console.WriteLine(cities.Where(x => x.IsCapital == true).Count());
+
+            Console.WriteLine("\nназви країн;");
+            cities.ToList().ForEach(x => Console.WriteLine(x.Country));
+
+            Console.WriteLine("\nкiлькiсть мiст iз населенням бiльше  1 000 000;");
+            Console.WriteLine(cities.Where(x => x.Population > 1000000).Count());
+
+            Console.WriteLine("\nназви мiст iз населенням менше  1 000 000;");
+            cities.Where(x => x.Population < 1000000).ToList().ForEach(x => Console.WriteLine(x.Name));
+
+            Console.WriteLine("\nназви країн, у яких назви мiст закiнчуютсья на букву 'w' у назвi мiста;");
+            cities.Where(x => x.Name.Last() == 'w').ToList().ForEach(x => Console.WriteLine(x.Country));
+
+            Console.WriteLine("\nкiлькiсть населення в найменш заселенiй столицi;");
+            Console.WriteLine(cities.Where(x => x.IsCapital == true).OrderBy(x => x.Population).First().Population);
+
+            Console.WriteLine("\nназви мiст, крiм перших i останнiх чотирьох;");
+            int rang = 4;
+            cities.ToList().GetRange(rang, cities.Count() - (rang + rang)).ForEach(x => Console.WriteLine(x.Name));
+
+            Console.WriteLine("\nмiнiмальне, максимальне та середнє значення;");
+            Console.WriteLine($"Min: {numbers.Min()} Avg: {numbers.Average()} Max: {numbers.Max()}");
+
+            Console.WriteLine("\nчи мiстить масив значення '-31'");
+            if (numbers.FirstOrDefault(x => x == -31) != default)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.WriteLine("\nостаннє парне значення.");
+            Console.WriteLine(numbers.Where(x => x % 2 == 0).Last());
 
             Console.ReadLine();
         }
