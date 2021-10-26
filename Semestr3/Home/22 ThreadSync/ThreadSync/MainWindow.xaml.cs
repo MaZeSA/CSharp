9050ccb8eade331d01ffbf48204c7da6c0bd6740
MaZeSA <lostfilmclient@gmail.com>
2021-10-26 17:30:06 +0300

    Thread

---
+++ Semestr3/Home/22 ThreadSync/ThreadSync/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThreadSync
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int count = 20;
+        Result Result = new Result(count);
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBox1.ItemsSource = Result.ListResult;
+
+            Thread t1 = new Thread(GetFactorial);
+            Thread t2 = new Thread(GetPow);
+            Thread t3 = new Thread(GetFibonacci);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+
+        private void GetFactorial()
+        {
+            for (int i = 1; i < count; i++)
+            {
+                Result.SetFactorial(i, Factorial(i));
+            }
+        }
+        private int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        private void GetPow()
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                Result.SetPow(i, (long)Math.Pow(i, 2));
+
+            }
+        }
+
+        private void GetFibonacci()
+        {
+            for (int i = 1; i < count; i++)
+            {
+                Result.SetFibonacci(i, Fibonacci(i));
+            }
+        }
+        public long Fibonacci(int n)
+        {
+            return n <= 0 ? 0 : n == 1 ? 1 : Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+    }
+}
---
+++ Semestr3/Home/22 ThreadSync/ThreadSync/Result.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSync
+{
+    public class Result //: INotifyPropertyChanged
+    {
+        // public event PropertyChangedEventHandler PropertyChanged;
+
+       public  ObservableCollection<ResultItem> ListResult { set; get; }
+
+        public Result(int count)
+        {
+            ListResult = new System.Collections.ObjectModel.ObservableCollection<ResultItem>();
+            for (int i = 0; i < count; i++)
+                ListResult.Add(new ResultItem(i+1));
+        }
+
+        public void SetFibonacci(int index, long value)
+        {
+            ListResult[index - 1].Fibonacci = value;
+        }
+        public void SetFactorial(int index, long value)
+        {
+            ListResult[index - 1].Factorial = value;
+        }
+        public void SetPow(int index, long value)
+        {
+            ListResult[index - 1].Pow = value;
+        }
+
+        public class ResultItem
+        {
+            public int Counter { set; get; }
+            public long Factorial { set; get; }
+            public long Pow { set; get; }
+            public long Fibonacci { set; get; }
+
+            public ResultItem(int c) => Counter = c;
+        }
+    }
+}
