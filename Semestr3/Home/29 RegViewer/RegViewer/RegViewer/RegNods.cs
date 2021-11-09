0c5f15ce53619d867f6c083cb00299ea22fbada6
MaZeSA <lostfilmclient@gmail.com>
2021-11-09 20:56:45 +0200

    RegViewer

---
+++ Semestr3/Home/29 RegViewer/RegViewer/RegViewer/RegNods.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegViewer
+{
+    public class RegNode : INotifyPropertyChanged
+    {
+        public ObservableCollection<RegNode> RegNodes { get; set; } = new ObservableCollection<RegNode>();
+        public RegistryKey RegistryKey { get; set; }
+
+        public string Name { get; set; }
+
+        public void ReadChildren()
+        {
+            try
+            {
+                var items = RegistryKey?.GetSubKeyNames();
+
+                foreach (var item in items)
+                {
+                    try
+                    {
+                        RegNodes.Add(new RegNode { RegistryKey = RegistryKey.OpenSubKey(item), Name = item });
+                    }
+                    catch (Exception ex) { System.Diagnostics.Debug.WriteLine(ex); }
+                }
+            }
+            catch (Exception ex) { System.Diagnostics.Debug.WriteLine(ex); }
+        }
+
+        private bool isExpanded;
+        public bool IsExpanded
+        {
+            get
+            {
+                ReadChildren();
+                return this.isExpanded;
+            }
+            set
+            {
+                if (value != this.isExpanded)
+                {
+                    this.isExpanded = value;
+
+                    NotifyPropertyChanged("IsExpanded");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+}
---
+++ Semestr3/Home/29 RegViewer/RegViewer/RegViewer/RegReader.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegViewer
+{
+    public class RegReader
+    {
+        public ObservableCollection<RegNode> RootNodes { get; set; } = new ObservableCollection<RegNode>()
+        {
+            new RegNode{RegistryKey = Registry.ClassesRoot, Name = Registry.ClassesRoot.Name},
+            new RegNode{RegistryKey = Registry.CurrentUser, Name = Registry.CurrentUser.Name},
+            new RegNode{RegistryKey = Registry.LocalMachine, Name = Registry.LocalMachine.Name},
+            new RegNode{RegistryKey = Registry.Users, Name = Registry.Users.Name},
+            new RegNode{RegistryKey = Registry.CurrentConfig, Name = Registry.CurrentConfig.Name},
+        };
+    }
+}
+
